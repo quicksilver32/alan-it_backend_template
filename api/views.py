0ca758db078493b61d8c148e675c6be65961ff82
Quicksilver32 <ameliya9@mail.ru>
2022-07-21 15:56:39 +0300

    add data

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
@@ -5,5 +6,15 @@ from rest_framework.views import APIView
 class GetData(APIView):
 
     def get(self, request):
-        # Ваш код для обработки данных
-        return JsonResponse({"response": "ok"})
+        response = []
+        flag = True
+        with open('./short.csv', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            for line in reader:
+                if flag:
+                    names = line
+                    flag = False
+                    continue
+                else:
+                    response.append({k:v for k, v in zip(names, line)})
+        return JsonResponse(response, safe=False)
